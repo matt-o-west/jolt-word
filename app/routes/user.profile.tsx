e6a07e0d46b7b02e14af885a70bc45f998073921
matt-o-west <matt.omalley.west@gmail.com>
2023-05-21 17:56:16 -0400

    working on profile route

--- app/routes/user.profile.tsx
+++ app/routes/user.profile.tsx
@@ -4,14 +4,20 @@ import { redirect } from '@remix-run/node'
 import type { ActionArgs } from '@remix-run/node'
 import { useContext } from 'react'
 import { badRequest } from '~/utils/request.server'
+import { validateUser, validatePassword } from '~/routes/login'
 import { Context } from '~/root'
 
 export const action = async ({ request }: ActionArgs) => {
   const form = await request.formData()
   const user = form.get('username')
-  const password = form.get('password')
+  const currentPassword = form.get('current-password')
+  const newPassword = form.get('new-password')
 
-  if (typeof user !== 'string' || typeof password !== 'string') {
+  if (
+    typeof user !== 'string' ||
+    typeof currentPassword !== 'string' ||
+    newPassword !== 'string'
+  ) {
     return badRequest({
       fieldErrors: null,
       fields: null,
@@ -20,6 +26,32 @@ export const action = async ({ request }: ActionArgs) => {
         timestamp: Date.now().toString(),
       },
     })
+  }
+
+  if (currentPassword === newPassword) {
+    return badRequest({
+      fieldErrors: null,
+      fields: null,
+      formError: {
+        message: 'New password cannot be the same as current password.',
+        timestamp: Date.now().toString(),
+      },
+    })
+  }
+
+  const fields = { user, currentPassword, newPassword }
+  const fieldErrors = {
+    user: validateUser(user),
+    currentPassword: validatePassword(currentPassword),
+    newPassword: validatePassword(newPassword),
+  }
+
+  if (
+    fieldErrors.user ||
+    fieldErrors.currentPassword ||
+    fieldErrors.newPassword
+  ) {
+    return badRequest({ fieldErrors, fields, formError: null })
   }
 
   return redirect('/login')
@@ -36,8 +68,8 @@ const Profile = () => {
         className={`flex flex-col justify-center items-center text-md py-1 mt-12 ${theme} desktop:max-w-2xl tablet:max-w-xl phone:max-w-315px phone:mx-auto`}
       >
         <h1>Profile</h1>
-        <Form>
-          <div className='mb-4'>
+        <Form action='/user/profile'>
+          <div className='mb-8'>
             <label
               htmlFor='username'
               className={`block mb-2 ${
@@ -77,12 +109,12 @@ const Profile = () => {
                 theme === 'light' ? 'text-primary.gray' : 'text-secondary.gray'
               }`}
             >
-              Password
+              Current Password
             </label>
             <input
               type='password'
-              id='password'
-              name='password'
+              id='current-password'
+              name='current-password'
               className={`w-full px-3 py-2 border-2 rounded-md focus:outline-none focus:border-purple ${
                 actionData?.fieldErrors?.password ? 'error-container' : null
               } ${
@@ -103,6 +135,47 @@ const Profile = () => {
               </p>
             )}
           </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='password'
+              className={`block mb-2 ${
+                theme === 'light' ? 'text-primary.gray' : 'text-secondary.gray'
+              }`}
+            >
+              New Password
+            </label>
+            <input
+              type='password'
+              id='new-password'
+              name='new-password'
+              className={`w-full px-3 py-2 border-2 rounded-md focus:outline-none focus:border-purple ${
+                actionData?.fieldErrors?.password ? 'error-container' : null
+              } ${
+                theme === 'light'
+                  ? 'bg-white border-secondary.gray'
+                  : 'bg-tertiary.black border-primary.gray'
+              }`}
+              required
+              defaultValue={actionData?.fields?.password}
+              aria-invalid={Boolean(actionData?.fieldErrors?.password)}
+              aria-errormessage={
+                actionData?.fieldErrors?.password ? 'password-error' : undefined
+              }
+            />
+            {actionData?.fieldErrors?.password && (
+              <p className='form-validation-error'>
+                {actionData.fieldErrors.password}
+              </p>
+            )}
+          </div>
+          <button
+            type='submit'
+            className={`btn btn-primary ${
+              theme === 'light' ? 'btn-light' : 'btn-dark'
+            }`}
+          >
+            Update
+          </button>
         </Form>
       </div>
     </>
