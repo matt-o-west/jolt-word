ce14fed9456b1b95eb164a8b1e9ff88151181466
matt-o-west <matt.omalley.west@gmail.com>
2023-07-30 19:54:18 -0400

    fiddling with server.ts file

--- server.ts
+++ server.ts
@@ -1,4 +1,12 @@
-import * as build from '@remix-run/dev/server-build'
 import { createRequestHandler } from '@vercel/remix/server'
+import * as build from '@remix-run/dev/server-build'
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV })
+export default createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+  getLoadContext() {
+    return {
+      nodeLoadContext: true,
+    }
+  },
+})
